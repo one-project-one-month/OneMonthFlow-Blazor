ecd3983d074ce392aa2d06cd9d56dd02fe3606d7
Myat Phone Thant <myatphonethant.dev@gmail.com>
2025-05-25 18:03:11 +0630

    feat: move files

--- OneMonthFlow.Shared/obj/Debug/net8.0/OneMonthFlow.Shared.AssemblyInfo.cs
+++ OneMonthFlow.Shared/obj/Debug/net8.0/OneMonthFlow.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneMonthFlow.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d807cb0904b1eca1f0872af898d14b8566671e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+249e53c4e9cf27240c60a48587e26abd311f705c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneMonthFlow.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneMonthFlow.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
